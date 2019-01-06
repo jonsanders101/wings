2c14ff9c9b90aeef324b8383a0ea7dd3574c572b
jonsanders101 <jonsanders101@aol.com>
2019-01-06 13:17:11 +0000

    Add get-movies route which sends all movie data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import express from 'express';
+import movies from './movies/allMovies';
+
+const api = express();
+
+api.get('/get-movies', (req, res) => {
+    res.send(movies);
+});
+
+api.listen(8080, () => {
+    console.log('Wings is now listening on port 8080');
+});
