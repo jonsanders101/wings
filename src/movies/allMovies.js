1a22c80597cc00fc5e462a49bd7c2e3a71c58e11
jonsanders101 <jonsanders101@aol.com>
2019-01-06 17:56:51 +0000

    Add Wings to movies

--- src/movies/allMovies.js
+++ src/movies/allMovies.js
@@ -1,6 +1,6 @@
 const FILLER_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-export default [{awardNumber: 1,
+export default [{name: "Wings", awardNumber: 1,
   releaseYear: 1927,
   seen: false,
   rating: null,
